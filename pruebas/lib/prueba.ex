8ebd98f146798b83f415e659ba64c5f7629f0201
gonzalo hernandez <gonzalo.hernandez@mimiquate.com>
2023-10-04 13:26:25 -0300

    mi primer commit

---
+++ pruebas/lib/prueba.ex
@@ -0,0 +1,8 @@
+defmodule Prueba do
+
+def upcase([]), do: []
+def upcase([first | rest]), do: [String.upcase(first) | upcase(rest)]
+# enves de usar ifs lo que hago es ir definiendo una funcion para cada caso, el primero caso es el que la lista que estoy pasando sea
+#vacia, el segundo caso es cuando la lista tiene cosas y cumple que es un string para poder hacerle String.upcase
+
+end
